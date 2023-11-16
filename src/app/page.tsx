71a44ca541ee06e83e2418e6515fce7dc61bedbb
MASAMIKI <masamiki.matsubara@amd-lab.com>
2023-11-16 19:56:50 +0900

    add button to download example ifc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Viewer from '@/components/viewer';
 
 export default function Home() {
-  return <Viewer />;
+  return (
+    <>
+      <button className="absolute bottom-0 left-0 ml-6 mb-4 z-[100] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        <a href="/example.ifc" target="_blank">
+          サンプルIFCファイルダウンロード
+        </a>
+      </button>
+      <Viewer />
+    </>
+  );
 }
